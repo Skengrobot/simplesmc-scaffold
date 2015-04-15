aa5b706c80a03fc563fa7c667ee725d324d1c193
Rudi Plesch <rudi.plesch@gmail.com>
2015-04-14 20:14:11 -0700

    function skeletons for linear gaussian model

--- src/main/java/simplesmc/lingauss/LinGaussParams.java
+++ src/main/java/simplesmc/lingauss/LinGaussParams.java
@@ -1,5 +1,7 @@
 package simplesmc.lingauss;
 
+import java.util.Random;
+
 /**
  *
  * Parameters for a simple linear Gaussian models
@@ -10,3 +12,46 @@ package simplesmc.lingauss;
  * @author Rudi Plesch (skengrobot@gmail.com)
  * 
 */
+
+public class LinGaussParams {
+	
+	private int observationDim;
+	private int stateSpaceDim;
+	
+  public double initialLogPr(int state) {
+	  // TODO
+	  return 0.0;
+  }
+  
+  public double[] sampleInitial(Random random) {
+	  // TODO
+	  return null;
+  }
+  
+  public double transitionLogPr(int currentState, int nextState) {
+	  // TODO
+	  return 0.0;
+  }
+  
+  public double[] sampleTransition(Random random, int currentState) {
+	  // TODO
+	  return null;
+  }
+  
+  public double emissionLogPr(int latentState, int emission) {
+	  // TODO
+	  return 0.0;
+  }
+
+  public double[] sampleEmission(Random random, int currentState) {
+	  return null;
+  }
+  
+  public int stateSpaceDimesion() {
+	  return this.stateSpaceDim;
+  }
+
+  public int observationDimesion() {
+	  return this.observationDim;
+  }
+}
