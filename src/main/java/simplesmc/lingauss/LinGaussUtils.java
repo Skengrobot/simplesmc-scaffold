360e64d75ee96e5b576c04761129ddfc16c2d360
Rudi Plesch <rudi.plesch@gmail.com>
2015-04-15 19:07:56 -0700

    Added a function to parse obrevations from files

---
+++ src/main/java/simplesmc/lingauss/LinGaussUtils.java
@@ -0,0 +1,38 @@
+package simplesmc.lingauss;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class LinGaussUtils {
+	
+	public static ArrayList<ArrayList<Double>> parseFile(String fileName){
+		
+		ArrayList<ArrayList<Double>> observations = new ArrayList<ArrayList<Double>>();
+		BufferedReader br = null;
+		try {
+			String line;
+			br = new BufferedReader(new FileReader(fileName));
+			
+			while ((line = br.readLine())!=null){
+				String[] strs = line.split(",");  
+				ArrayList<Double> state = new ArrayList<>();
+				for (String s : strs)
+					state.add(Double.parseDouble(s));
+				observations.add(state);
+			}
+		}
+		catch (IOException e){e.printStackTrace();}
+		finally {
+			try
+				{if(br!=null){br.close();}}
+			catch (IOException e){e.printStackTrace();}
+		}
+		
+		return observations;
+	}
+}
