12a8448f1f1ad16963f43f75a32715ee31145fe0
Rudi Plesch <rudi.plesch@gmail.com>
2015-04-19 15:05:01 -0700

    Unit test for fancy SMC

--- src/test/java/simplesmc/TestFancySMC.java
+++ src/test/java/simplesmc/TestFancySMC.java
@@ -1,11 +1,14 @@
 package simplesmc;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Test;
 
+import bayonet.smc.ParticlePopulation;
+import simplesmc.fancysmc.fancySMCalgorithm;
 import simplesmc.hmm.HMMProblemSpecification;
 import simplesmc.hmm.HMMUtils;
 import simplesmc.hmm.ToyHMMParams;
@@ -18,7 +21,7 @@ public class TestFancySMC {
 		Random random = new Random(1);
 		ToyHMMParams hmmParams = new ToyHMMParams(5);
     
-		Pair<List<Integer>, List<Integer>> generated = HMMUtils.generate(random, hmmParams, 100);
+		Pair<List<Integer>, List<Integer>> generated = HMMUtils.generate(random, hmmParams, 200);
 		List<Integer> observations = generated.getRight();
     
 		System.out.println("exact = " + HMMUtils.exactDataLogProbability(hmmParams, observations));
@@ -26,7 +29,8 @@ public class TestFancySMC {
 		HMMProblemSpecification proposal = new HMMProblemSpecification(hmmParams, observations);
     
 		SMCOptions options = new SMCOptions();
-		SMCAlgorithm<Integer> smc = new SMCAlgorithm<>(proposal, options);
-		System.out.println("estimate = " + smc.sample().logNormEstimate());
+		fancySMCalgorithm<Integer> smc = new fancySMCalgorithm<>(proposal, options, 10, 4);
+		Pair<ParticlePopulation<Integer>, ArrayList<Double>> output = smc.fancySample();
+		System.out.println("Final log likelihood = " + output.getLeft().logNormEstimate());
 	}
 }
