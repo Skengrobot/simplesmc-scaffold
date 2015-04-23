5e6fbbf1b2943fbdf779f3cf26187fb7909083fa
Rudi Plesch <rudi.plesch@gmail.com>
2015-04-22 17:24:31 -0700

    changed linear gaussian to actually change parameters

--- data-generators/iid_gaussian_changepoint.py
+++ data-generators/iid_gaussian_changepoint.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import math
 import numpy
 
 
@@ -8,12 +9,12 @@ if len(sys.argv) < 2:
     print 'Enter an iteration count (length of process) and file name to output to'
 
 mean = 0
-variance = 0.1
+variance = 0.01
 
 outfile = open(sys.argv[2],'w')
 
 for i in range(int(sys.argv[1])):
-    roll = numpy.random.normal();
+    roll = numpy.random.normal(mean, math.sqrt(variance));
     outfile.write(str(roll)+'\n')
 
 outfile.close()
